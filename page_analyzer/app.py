d718130146f72a74ac108202d268e1d1d1c315f9
WhiteA <crazy032@mail.ru>
2025-08-13 19:15:05 +0300

    step 2. lint-fix

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -7,6 +7,6 @@ load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
