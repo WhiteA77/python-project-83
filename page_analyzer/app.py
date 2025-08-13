e226a67934c2300bb6183f3fd1d140ebde1e28b1
WhiteA <crazy032@mail.ru>
2025-08-13 19:13:32 +0300

    step 2. Шаблонизатор

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,20 +1,12 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template
 
 load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-@app.route("/")
+@app.route('/')
 def index():
-    return """
-    Семья за завтраком.<br><br>
-    Муж: "Дорогая, ты почему кофе остывший налила?"<br>
-    Жена (не отрываясь от ноутбука): "Это не баг, это фича. Оптимизировала время на остывание."<br><br>
-    Муж: "А тост чёрствый..."<br>
-    Жена: "У него просто повышенная fault tolerance. Ешь в два раза медленнее — нагрузка на ЖКТ распределится лучше."<br><br>
-    Ребёнок: "Мам, а молоко прокисло!"<br>
-    Жена (задумчиво): "Странно... Вчера ещё было 'Last stable version'..."
-    """
+    return render_template('index.html')
